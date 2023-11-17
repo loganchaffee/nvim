c95173d2f5c313145320e7b22dd1b87a5495ca2b
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2023-11-16 16:56:18 -0800

    Open files in new tab by default

--- lua/logan/plugins/nvim-tree.lua
+++ lua/logan/plugins/nvim-tree.lua
@@ -10,7 +10,21 @@ return {
 		vim.opt.termguicolors = true
 
 		-- empty setup using defaults
-		require("nvim-tree").setup({
+		local nvim_tree = require("nvim-tree")
+		local api = require("nvim-tree.api")
+
+		nvim_tree.setup({
+			on_attach = function(bufnr)
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				vim.keymap.set("n", "<CR>", api.node.open.tab, opts("Open: New Tab"))
+			end,
+			open_on_tab = true,
+			update_focused_file = {
+				enable = true,
+			},
 			actions = {
 				open_file = {
 					quit_on_open = true,
--- lua/logan/remap.lua
+++ lua/logan/remap.lua
@@ -1,18 +1,18 @@
--- General 
+-- General
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.opt.clipboard = "unnamedplus" -- Yank to clipboard
 
 -- Telescope (fuzzy finder)
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>st', builtin.live_grep, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>st", builtin.live_grep, {})
 
 -- Lazy (plugin manager)
-vim.keymap.set('n', '<leader>L', vim.cmd.Lazy)
+vim.keymap.set("n", "<leader>L", vim.cmd.Lazy)
 
 -- Mason (lsp/tooling manager)
-vim.keymap.set('n', '<leader>M', vim.cmd.Mason)
+vim.keymap.set("n", "<leader>M", vim.cmd.Mason)
 
 -- Nvim Tree
-vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
