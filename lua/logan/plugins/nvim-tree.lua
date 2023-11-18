35d7128c1abf89289ef85650a710d9b42b7052e0
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2023-11-17 17:27:50 -0800

    Use default mappings first

--- lua/logan/plugins/nvim-tree.lua
+++ lua/logan/plugins/nvim-tree.lua
@@ -12,13 +12,15 @@ return {
 		local nvim_tree = require("nvim-tree")
 
 		nvim_tree.setup({
-			-- Open file in new tab, go to tab if already open
 			on_attach = function(bufnr)
 				local function opts(desc)
 					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 				end
 				local ok, api = pcall(require, "nvim-tree.api")
 				assert(ok, "api module is not found")
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+				-- Open file in new tab, go to tab if already open
 				vim.keymap.set("n", "<CR>", api.node.open.tab_drop, opts("Tab drop"))
 			end,
 			update_focused_file = {
