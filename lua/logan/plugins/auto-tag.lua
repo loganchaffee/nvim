a4a941494e261080c6f18b4919653ddf6a4dde2b
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2023-11-24 10:21:33 -0800

    Add auto closing tags

---
+++ lua/logan/plugins/auto-tag.lua
@@ -0,0 +1,20 @@
+return {
+	"windwp/nvim-ts-autotag",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			autotag = {
+				enable = true,
+			},
+		})
+
+		vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+			underline = true,
+			virtual_text = {
+				spacing = 5,
+				severity_limit = "Warning",
+			},
+			update_in_insert = true,
+		})
+	end,
+}
