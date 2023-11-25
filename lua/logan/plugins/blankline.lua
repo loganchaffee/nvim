ff988c0c06d12986ff2af46eb62abb1843f3cfb9
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2023-11-24 19:53:27 -0800

    Edit indent line color

--- lua/logan/plugins/blankline.lua
+++ lua/logan/plugins/blankline.lua
@@ -2,10 +2,20 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
 	config = function()
+		vim.cmd([[highlight IndentBlanklineIndent1 guifg=#bd93f9 gui=nocombine]])
+
+		local hooks = require("ibl.hooks")
+		-- create the highlight groups in the highlight setup hook, so they are reset
+		-- every time the colorscheme changes
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "Gray", { fg = "#404040" })
+		end)
+
 		require("ibl").setup({
 			indent = {
 				char = "▏",
 				tab_char = "▏",
+				highlight = { "Gray" },
 			},
 			scope = { enabled = true },
 			exclude = {
