72b2e40256230911b4e39e7e770d987081343412
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2024-03-26 14:23:34 -0700

    Clean up

--- lua/logan/plugins/colorscheme.lua
+++ lua/logan/plugins/colorscheme.lua
@@ -1,22 +1,3 @@
-local function makeTransParent()
-	vim.cmd("au ColorScheme * hi Normal ctermbg=none guibg=none")
-	vim.cmd("au ColorScheme * hi SignColumn ctermbg=none guibg=none")
-	vim.cmd("au ColorScheme * hi NormalNC ctermbg=none guibg=none")
-	vim.cmd("au ColorScheme * hi MsgArea ctermbg=none guibg=none")
-	vim.cmd("au ColorScheme * hi TelescopeBorder ctermbg=none guibg=none")
-	vim.cmd("au ColorScheme * hi NvimTreeNormal ctermbg=none guibg=none")
-	vim.cmd("let &fcs='eob: '")
-end
-
--- return {
--- 	"lunarvim/colorschemes",
--- 	priority = 1000,
--- 	config = function()
--- 		makeTransParent()
--- 		vim.cmd("colorscheme darkplus")
--- 	end,
--- }
-
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
--- lua/logan/plugins/tree-sitter.lua
+++ lua/logan/plugins/tree-sitter.lua
@@ -1,52 +1,17 @@
-return { -- Highlight, edit, and navigate code
+-- Highlight, edit, and navigate code
+return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function(_, opts)
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-		parser_config.gotmpl = {
-			install_info = {
-				url = "https://github.com/ngalaiko/tree-sitter-go-template",
-				files = { "src/parser.c" },
-			},
-			filetype = "gotmpl",
-			used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
-		}
-
-		---@diagnostic disable-next-line: missing-fields
+	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"html",
-				"javascript",
-				"typescript",
-				"tsx",
-				"json",
-				"lua",
-				"go",
-				"c",
-				"vim",
-				"vimdoc",
-				"query",
-				"python",
-				"css",
-			},
+			ensure_installed = "all",
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
 				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
 				additional_vim_regex_highlighting = { "ruby" },
 			},
 			indent = { enable = true, disable = { "ruby" } },
 		})
-
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	end,
 }
