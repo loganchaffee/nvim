08d2caab8b33f7ec98d1ce100f43765b21efc90e
Logan Chaffee <79607299+loganchaffee@users.noreply.github.com>
2024-06-06 12:41:23 -0700

    Add comment toggle support in jsx

--- lua/logan/plugins/comment.lua
+++ lua/logan/plugins/comment.lua
@@ -1,8 +1,10 @@
 return {
 	"numToStr/Comment.nvim",
+	dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 	lazy = false,
 	config = function()
 		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 			toggler = {
 				---Line-comment toggle keymap
 				line = "<leader>/",
